054288c83bfc15e668b5bb6f000d0b72850aa641
Morhaf96 <morhaf>
2019-10-24 17:37:56 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace PodcastProjekt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("YOOOOO WASSUP!");
+
         }
     }
 }
