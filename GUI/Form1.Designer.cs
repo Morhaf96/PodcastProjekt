f4914e632a7562ed30a550e12789b972f1bb8d01
Morhaf96 <morhaf>
2019-10-28 14:00:19 +0100

    Ändringar i desginen

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -47,6 +47,8 @@
             this.lblAvsnitt = new System.Windows.Forms.Label();
             this.lblDetaljer = new System.Windows.Forms.Label();
             this.lvlKateogir = new System.Windows.Forms.Label();
+            this.lblUppdateringsintervall = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPod)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,9 +56,9 @@
             // 
             this.cmbKategori.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbKategori.FormattingEnabled = true;
-            this.cmbKategori.Location = new System.Drawing.Point(12, 247);
+            this.cmbKategori.Location = new System.Drawing.Point(86, 219);
             this.cmbKategori.Name = "cmbKategori";
-            this.cmbKategori.Size = new System.Drawing.Size(207, 21);
+            this.cmbKategori.Size = new System.Drawing.Size(246, 21);
             this.cmbKategori.TabIndex = 0;
             // 
             // dgvPod
@@ -65,7 +67,7 @@
             this.dgvPod.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPod.Location = new System.Drawing.Point(12, 28);
             this.dgvPod.Name = "dgvPod";
-            this.dgvPod.Size = new System.Drawing.Size(410, 151);
+            this.dgvPod.Size = new System.Drawing.Size(466, 151);
             this.dgvPod.TabIndex = 3;
             // 
             // lbAvsnitt
@@ -73,15 +75,15 @@
             this.lbAvsnitt.FormattingEnabled = true;
             this.lbAvsnitt.Location = new System.Drawing.Point(12, 294);
             this.lbAvsnitt.Name = "lbAvsnitt";
-            this.lbAvsnitt.Size = new System.Drawing.Size(410, 186);
+            this.lbAvsnitt.Size = new System.Drawing.Size(466, 186);
             this.lbAvsnitt.TabIndex = 4;
             // 
             // lvKat
             // 
             this.lvKat.HideSelection = false;
-            this.lvKat.Location = new System.Drawing.Point(487, 31);
+            this.lvKat.Location = new System.Drawing.Point(552, 31);
             this.lvKat.Name = "lvKat";
-            this.lvKat.Size = new System.Drawing.Size(390, 148);
+            this.lvKat.Size = new System.Drawing.Size(325, 148);
             this.lvKat.TabIndex = 5;
             this.lvKat.UseCompatibleStateImageBehavior = false;
             // 
@@ -90,19 +92,19 @@
             this.tlpDetalj.ColumnCount = 2;
             this.tlpDetalj.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tlpDetalj.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpDetalj.Location = new System.Drawing.Point(487, 294);
+            this.tlpDetalj.Location = new System.Drawing.Point(552, 294);
             this.tlpDetalj.Name = "tlpDetalj";
             this.tlpDetalj.RowCount = 2;
             this.tlpDetalj.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53F));
             this.tlpDetalj.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47F));
-            this.tlpDetalj.Size = new System.Drawing.Size(390, 186);
+            this.tlpDetalj.Size = new System.Drawing.Size(325, 186);
             this.tlpDetalj.TabIndex = 6;
             // 
             // lblKategori
             // 
             this.lblKategori.AutoSize = true;
             this.lblKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblKategori.Location = new System.Drawing.Point(12, 228);
+            this.lblKategori.Location = new System.Drawing.Point(15, 220);
             this.lblKategori.Name = "lblKategori";
             this.lblKategori.Size = new System.Drawing.Size(65, 16);
             this.lblKategori.TabIndex = 7;
@@ -111,16 +113,16 @@
             // 
             // tbUrl
             // 
-            this.tbUrl.Location = new System.Drawing.Point(12, 205);
+            this.tbUrl.Location = new System.Drawing.Point(56, 185);
             this.tbUrl.Name = "tbUrl";
-            this.tbUrl.Size = new System.Drawing.Size(207, 20);
+            this.tbUrl.Size = new System.Drawing.Size(276, 20);
             this.tbUrl.TabIndex = 8;
             // 
             // lblUrl
             // 
             this.lblUrl.AutoSize = true;
             this.lblUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUrl.Location = new System.Drawing.Point(12, 186);
+            this.lblUrl.Location = new System.Drawing.Point(15, 186);
             this.lblUrl.Name = "lblUrl";
             this.lblUrl.Size = new System.Drawing.Size(38, 16);
             this.lblUrl.TabIndex = 9;
@@ -128,25 +130,25 @@
             // 
             // btnLaggTillPod
             // 
-            this.btnLaggTillPod.Location = new System.Drawing.Point(264, 205);
+            this.btnLaggTillPod.Location = new System.Drawing.Point(338, 189);
             this.btnLaggTillPod.Name = "btnLaggTillPod";
-            this.btnLaggTillPod.Size = new System.Drawing.Size(115, 23);
+            this.btnLaggTillPod.Size = new System.Drawing.Size(140, 23);
             this.btnLaggTillPod.TabIndex = 10;
             this.btnLaggTillPod.Text = "Lägg till podcast";
             this.btnLaggTillPod.UseVisualStyleBackColor = true;
             // 
             // btnTaBortPod
             // 
-            this.btnTaBortPod.Location = new System.Drawing.Point(264, 245);
+            this.btnTaBortPod.Location = new System.Drawing.Point(338, 219);
             this.btnTaBortPod.Name = "btnTaBortPod";
-            this.btnTaBortPod.Size = new System.Drawing.Size(115, 23);
+            this.btnTaBortPod.Size = new System.Drawing.Size(140, 21);
             this.btnTaBortPod.TabIndex = 11;
-            this.btnTaBortPod.Text = "Ta bort podcast";
+            this.btnTaBortPod.Text = "Ta bort markerad podcast";
             this.btnTaBortPod.UseVisualStyleBackColor = true;
             // 
             // tbKategori
             // 
-            this.tbKategori.Location = new System.Drawing.Point(622, 186);
+            this.tbKategori.Location = new System.Drawing.Point(748, 182);
             this.tbKategori.Name = "tbKategori";
             this.tbKategori.Size = new System.Drawing.Size(125, 20);
             this.tbKategori.TabIndex = 12;
@@ -156,7 +158,7 @@
             // 
             this.lblKatnamn.AutoSize = true;
             this.lblKatnamn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblKatnamn.Location = new System.Drawing.Point(498, 190);
+            this.lblKatnamn.Location = new System.Drawing.Point(572, 189);
             this.lblKatnamn.Name = "lblKatnamn";
             this.lblKatnamn.Size = new System.Drawing.Size(118, 16);
             this.lblKatnamn.TabIndex = 13;
@@ -164,9 +166,9 @@
             // 
             // btnLaggTillKat
             // 
-            this.btnLaggTillKat.Location = new System.Drawing.Point(487, 220);
+            this.btnLaggTillKat.Location = new System.Drawing.Point(552, 216);
             this.btnLaggTillKat.Name = "btnLaggTillKat";
-            this.btnLaggTillKat.Size = new System.Drawing.Size(129, 24);
+            this.btnLaggTillKat.Size = new System.Drawing.Size(108, 24);
             this.btnLaggTillKat.TabIndex = 14;
             this.btnLaggTillKat.Text = "Lägg till kategori";
             this.btnLaggTillKat.UseVisualStyleBackColor = true;
@@ -174,18 +176,18 @@
             // 
             // btnTaBortKat
             // 
-            this.btnTaBortKat.Location = new System.Drawing.Point(748, 220);
+            this.btnTaBortKat.Location = new System.Drawing.Point(771, 217);
             this.btnTaBortKat.Name = "btnTaBortKat";
-            this.btnTaBortKat.Size = new System.Drawing.Size(129, 23);
+            this.btnTaBortKat.Size = new System.Drawing.Size(106, 23);
             this.btnTaBortKat.TabIndex = 15;
             this.btnTaBortKat.Text = "Ta bort kategori";
             this.btnTaBortKat.UseVisualStyleBackColor = true;
             // 
             // btnSparaKat
             // 
-            this.btnSparaKat.Location = new System.Drawing.Point(622, 220);
+            this.btnSparaKat.Location = new System.Drawing.Point(666, 217);
             this.btnSparaKat.Name = "btnSparaKat";
-            this.btnSparaKat.Size = new System.Drawing.Size(120, 23);
+            this.btnSparaKat.Size = new System.Drawing.Size(98, 23);
             this.btnSparaKat.TabIndex = 16;
             this.btnSparaKat.Text = "Spara kategori";
             this.btnSparaKat.UseVisualStyleBackColor = true;
@@ -214,7 +216,7 @@
             // 
             this.lblDetaljer.AutoSize = true;
             this.lblDetaljer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDetaljer.Location = new System.Drawing.Point(487, 275);
+            this.lblDetaljer.Location = new System.Drawing.Point(549, 275);
             this.lblDetaljer.Name = "lblDetaljer";
             this.lblDetaljer.Size = new System.Drawing.Size(67, 16);
             this.lblDetaljer.TabIndex = 19;
@@ -224,11 +226,29 @@
             // 
             this.lvlKateogir.AutoSize = true;
             this.lvlKateogir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvlKateogir.Location = new System.Drawing.Point(490, 11);
+            this.lvlKateogir.Location = new System.Drawing.Point(546, 9);
             this.lvlKateogir.Name = "lvlKateogir";
             this.lvlKateogir.Size = new System.Drawing.Size(70, 16);
             this.lvlKateogir.TabIndex = 20;
             this.lvlKateogir.Text = "Kategori:";
+            // 
+            // lblUppdateringsintervall
+            // 
+            this.lblUppdateringsintervall.AutoSize = true;
+            this.lblUppdateringsintervall.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUppdateringsintervall.Location = new System.Drawing.Point(15, 250);
+            this.lblUppdateringsintervall.Name = "lblUppdateringsintervall";
+            this.lblUppdateringsintervall.Size = new System.Drawing.Size(139, 16);
+            this.lblUppdateringsintervall.TabIndex = 21;
+            this.lblUppdateringsintervall.Text = "Uppdateringsintervall:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(160, 245);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 22;
             // 
             // Form1
             // 
@@ -236,6 +256,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(889, 493);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblUppdateringsintervall);
             this.Controls.Add(this.lvlKateogir);
             this.Controls.Add(this.lblDetaljer);
             this.Controls.Add(this.lblAvsnitt);
@@ -286,6 +308,8 @@
         private System.Windows.Forms.Label lblAvsnitt;
         private System.Windows.Forms.Label lblDetaljer;
         private System.Windows.Forms.Label lvlKateogir;
+        private System.Windows.Forms.Label lblUppdateringsintervall;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
