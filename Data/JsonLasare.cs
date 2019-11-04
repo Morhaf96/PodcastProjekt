5d2ea912ea8496e97b4a5c95fbafcea13277720e
Morhaf96 <morhaf>
2019-11-04 18:39:42 +0100

    Kodade om JsonLasare och -skrivare för att dom inte laddes upp på förra pushen för nån konstig anledning

---
+++ Data/JsonLasare.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using PodcastProjekt.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodcastProjekt.Data
+{
+    class JsonLasare
+    {
+        private readonly JsonSerializer jsonSerializer;
+        private readonly string filnamn;
+        
+        public JsonLasare(string filnamn)
+        {
+            this.filnamn = filnamn;
+            jsonSerializer = new JsonSerializer { TypeNameHandling = TypeNameHandling.All };
+        }
+        public List<Kategori> lasKategoriLista()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filnamn))
+                {
+                    using (JsonTextReader jtr = new JsonTextReader(sr))
+                    {
+                        return jsonSerializer.Deserialize<List<Kategori>>(jtr);
+                    }
+                }
+            }
+            catch
+            {
+                return new List<Kategori>();
+            }
+        }
+
+        public List<Podcast> lasPodcastLista()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filnamn))
+                {
+                    using (JsonTextReader jtr = new JsonTextReader(sr))
+                    {
+                        return jsonSerializer.Deserialize<List<Podcast>>(jtr);
+                    }
+                }
+            }
+            catch
+            {
+                return new List<Podcast>();
+            }
+        }
+    }
+}
---
+++ Data/JsonSkrivare.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using PodcastProjekt.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodcastProjekt.Data
+{
+    class JsonSkrivare
+    {
+        private readonly JsonSerializer jsonSerializer;
+        private readonly string filnamn;
+       
+        public JsonSkrivare(string filnamn)
+        {
+            jsonSerializer = new JsonSerializer { TypeNameHandling = TypeNameHandling.All };
+            this.filnamn = filnamn;
+        }
+        public void sparaKategorier(List<Kategori> kategoriLista)
+        {
+            using (StreamWriter sw = new StreamWriter(filnamn))
+            {
+                using (JsonTextWriter jtw = new JsonTextWriter(sw))
+                {
+                    jsonSerializer.Serialize(jtw, kategoriLista);
+                }
+            }
+        }
+        public void sparaPodcasts(List<Podcast> podcastLista)
+        {
+            using (StreamWriter sw = new StreamWriter(filnamn))
+            {
+                using (JsonTextWriter jtw = new JsonTextWriter(sw))
+                {
+                    jsonSerializer.Serialize(jtw, podcastLista);
+                }
+            }
+        }
+    }
+}
